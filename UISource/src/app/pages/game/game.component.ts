c21930aacf8263a7b5f1a6f96d080eafd8fe395a
Felipe <you_m4ster@hotmail.com>
2018-03-18 23:48:27 -0500

    adding popup to add points

--- UISource/src/app/pages/game/game.component.ts
+++ UISource/src/app/pages/game/game.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { GameService } from 'app/@core/data/game.service';
 import { forEach } from '@angular/router/src/utils/collection';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngx-game',
@@ -11,8 +12,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class GameComponent implements OnInit  {
   public id: number;
   public game: any = {};
-
+  public dataPointToAdd: any = {};
+  public pointList: Array<String> = ['1', '5', '10', '50'];
+  @ViewChild('engineTemplate') private modalTemplate: TemplateRef<any>;;
+  //@ViewChild("engineTemplate") private engineModal: TemplateRef<any>;
+  //dialog: NgbModalRef | null;
   constructor(        
+    private modalService: NgbModal,
     protected router: Router,
     private route: ActivatedRoute,
     private gameService: GameService) {
@@ -23,9 +29,25 @@ export class GameComponent implements OnInit  {
   ngOnInit(): void {
     const self = this;
     this.gameService.getGame(this.id).subscribe(res => {
+      if(res.players != null)
+        if(res.players[0] == null)
+          res.players = [];
+      //console.log('res: ', res);
       self.game = res;
     });
   }
+  openModel():Boolean{
+    this.modalService.open(this.modalTemplate, {size: 'lg'}).result.then((result) => {
+      //this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      //this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });    
+    return false;
+  }
 
+  onChangePointCombo(point: string) { 
+    console.log('point: ', point)
+    this.dataPointToAdd.points = point;
+  }
 
 }
